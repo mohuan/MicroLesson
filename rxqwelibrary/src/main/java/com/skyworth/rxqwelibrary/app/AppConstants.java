d4e035e1294223bae4c619bdaaa16ab8bf88fc3e
weidingqiang <weidingqiang@163.com>
2019-05-28 09:19:25 +0800

    创建项目

---
+++ rxqwelibrary/src/main/java/com/skyworth/rxqwelibrary/app/AppConstants.java
@@ -0,0 +1,73 @@
+package com.skyworth.rxqwelibrary.app;
+
+import java.io.File;
+
+/**
+ * 常用的变量值
+ *
+ * @author skyworth
+ *
+ *         2015年6月2日
+ */
+public class AppConstants {
+
+    public static String PARENT_FOLD_PATH = "";
+    //缓存相关
+    /**
+     * 缓存地址
+     */
+    public static String CACHE_PATH = "";
+
+    /**
+     * logs 文件
+     * @param string
+     */
+    public static String LOGS_PATH = "";
+
+    public static void initPath(String string){
+        PARENT_FOLD_PATH = string+ File.separator;
+        CACHE_PATH = PARENT_FOLD_PATH;
+
+        DOWNLOAD_PATH = PARENT_FOLD_PATH + "download" + File.separator;
+        LOGS_PATH = PARENT_FOLD_PATH + "logs" + File.separator;
+
+        //download
+        RECORD_DOWNLOAD_PATH = DOWNLOAD_PATH + "audio" + File.separator;
+        FILE_DOWNLOAD_PATH = DOWNLOAD_PATH + "files" + File.separator;
+        APK_DOWNLOAD_PATH = DOWNLOAD_PATH + "apk" + File.separator;
+
+    }
+
+    //--------------------------------------------------------------------------//
+    //下载相关
+    /**
+     * 下载文件的存储地址
+     */
+    public static String DOWNLOAD_PATH = "";
+    /**
+     * 录音文件下载存储地址
+     */
+    public static String RECORD_DOWNLOAD_PATH = "";
+    /**
+     * 普通文件下载存储地址
+     */
+    public static String FILE_DOWNLOAD_PATH = "";
+
+    //-----------------------------------------------//
+    /**
+     * APK文件下载地址
+     */
+    public static String APK_DOWNLOAD_PATH = "";
+
+    // -----------------文件后缀名-------------
+    /**
+     * 音频文件后缀名
+     */
+    public static final String AUDIO_FILE_SUFFIX = ".mp3";
+    /**
+     * 音频文件后缀名
+     */
+    public static final String SPEECHS_FILE_SUFFIX = ".spx";
+
+    //------------------------------------------------------//
+}
---
+++ rxqwelibrary/src/main/java/com/skyworth/rxqwelibrary/base/SimpleFragmentActivity.java
@@ -0,0 +1,73 @@
+package com.skyworth.rxqwelibrary.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.umeng.analytics.MobclickAgent;
+import com.skyworth.rxqwelibrary.managers.AppManagers;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * 作者：skyworth on 2017/7/10 11:11
+ * 邮箱：dqwei@iflytek.com
+ */
+
+public abstract class SimpleFragmentActivity extends SupportAutoActivity{
+
+    //退出事件
+    private long exitTime = 0;
+
+    protected Activity mContext;
+    private Unbinder mUnBinder;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(getLayout());
+        mUnBinder = ButterKnife.bind(this);
+        mContext = this;
+        onViewCreated();
+        AppManagers.getInstance().addActivity(this);
+        initEventAndData();
+    }
+
+    protected void onViewCreated() {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        AppManagers.getInstance().killActivity(this);
+        mUnBinder.unbind();
+    }
+
+    protected abstract int getLayout();
+    protected abstract void initEventAndData();
+
+    //-------------------------------------------------------------------//
+    public void exit() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(), "再按一次退出程序",
+                    Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            AppManagers.getInstance().exit(this);
+        }
+    }
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+}
